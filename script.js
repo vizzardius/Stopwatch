d954caf8dcb1c7ac26c9a9916e4c29e0a1885c61
vizzardius <vizzardiusgh14@gmail.com>
2025-12-27 23:16:44 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+let minutes = document.querySelector(".minutes")
+let seconds = document.querySelector(".seconds")
+let miliseconds = document.querySelector(".miliseconds")
+let startButton = document.querySelector(".startButton")
+let stopButton = document.querySelector(".stopButton")
+let resetButton = document.querySelector(".resetButton")
+
+let milisecondsValue = +miliseconds.textContent
+let secondsValue = +seconds.textContent
+let minutesValue = +minutes.textContent
+let stopwatchInterval = null
+
+
+
+startButton.addEventListener("click",()=>{
+   if(stopwatchInterval !== null) return;
+   stopButton.textContent = "Pause"
+   stopwatchInterval = setInterval(()=>{
+      if(milisecondsValue>99){
+         milisecondsValue = 0
+         secondsValue ++
+      }
+      if(secondsValue > 59){
+         secondsValue = 0
+         minutesValue++
+      }
+      milisecondsValue ++
+      miliseconds.textContent = milisecondsValue
+      seconds.textContent = secondsValue
+      minutes.textContent = minutesValue
+   },10) 
+})
+
+stopButton.addEventListener("click",()=>{
+   clearInterval(stopwatchInterval)
+   stopwatchInterval = null
+   startButton.textContent = "Continue"
+})
+
+resetButton.addEventListener("click",()=>{
+   window.location.href = "index.html"
+})
